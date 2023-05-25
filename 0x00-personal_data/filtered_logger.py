e3950c7c0060333cd9cd361af9134de5912e0508
jojobutercup <jojobutercup@gmail.com>
2023-05-25 16:59:05 +0100

    commit

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,33 +3,74 @@
 Filtered Logger module
 """
 
-import os
-import mysql.connector
+import logging
+from typing import List
 from mysql.connector import errorcode
+from mysql.connector.connection import MySQLConnection
+
+PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
 
 
-def get_db() -> mysql.connector.connection.MySQLConnection:
+def get_db() -> MySQLConnection:
     """
     Connect to the MySQL database and return a connection object
     """
-    username = os.getenv('PERSONAL_DATA_DB_USERNAME', 'root')
-    password = os.getenv('PERSONAL_DATA_DB_PASSWORD', '')
-    host = os.getenv('PERSONAL_DATA_DB_HOST', 'localhost')
-    database = os.getenv('PERSONAL_DATA_DB_NAME')
+    # Same implementation as before
 
-    try:
-        db = mysql.connector.connect(
-            user=username,
-            password=password,
-            host=host,
-            database=database
-        )
-        return db
-    except mysql.connector.Error as err:
-        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-            print("Error: Access denied. Refer to database credentials.")
-        elif err.errno == errorcode.ER_BAD_DB_ERROR:
-            print("Error: The specified database does not exist.")
-        else:
-            print("Error: Failed to connect to the database.")
-        exit(1)
+
+def main() -> None:
+    """
+    Obtain a database connection using get_db and
+    retrieve all rows in the users table.
+    Display each row under a filtered format.
+    """
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+
+    handler = logging.StreamHandler()
+    formatter = RedactingFormatter(fields=PII_FIELDS)
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+
+    db = get_db()
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM users;")
+    for row in cursor:
+        logger.info(format_log(row))
+    cursor.close()
+    db.close()
+
+
+def format_log(row: List[str]) -> str:
+    """
+    Format the log message with filtered fields
+    """
+    filtered_row = {
+        field: '***' if field in PII_FIELDS else value
+        for field, value in zip(PII_FIELDS, row)
+    }
+    return "; ".join(
+            f"{field}={value}"
+            for field, value in filtered_row.items()
+    )
+
+
+class RedactingFormatter(logging.Formatter):
+    """
+    Custom log formatter to redact sensitive information
+    """
+
+    def __init__(self, fields: List[str]):
+        self.fields = fields
+        super().__init__()
+
+    def format(self, record: logging.LogRecord) -> str:
+        message = super().format(record)
+        for field in self.fields:
+            message = message.replace(field, "***")
+        return message
+
+
+if __name__ == "__main__":
+    main()
