8cce64ee4d790699e6cd73a34a7227d742d710bd
jojobutercup <jojobutercup@gmail.com>
2023-05-25 12:41:53 +0100

    Commit

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,21 +3,22 @@
 Filtered Logger module
 """
 
+from typing import List
 import re
 
 
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(
+        fields: List[str],
+        redaction: str,
+        message: str,
+        separator: str
+) -> str:
     """
-    Obfuscates specified fields in a log message
+    Replace field values in a log message with a redaction string
     """
-    return re.sub(r'(?<=\b' + separator + '|^)(' + '|'.join(fields) + ')=.*?(?=' + separator + '|$)', lambda match: match.group(1) + '=' + redaction, message)
-
-
-if __name__ == "__main__":
-    fields = ["password", "date_of_birth"]
-    messages = ["name=egg;email=eggmin@eggsample.com;password=eggcellent;date_of_birth=12/12/1986;",
-                "name=bob;email=bob@dylan.com;password=bobbycool;date_of_birth=03/04/1993;"]
-
-    for message in messages:
-        print(filter_datum(fields, 'xxx', message, ';'))
-
+    regex = r'(' + '|'.join(fields) + r')=[^{}]+'.format(separator)
+    return re.sub(
+            regex,
+            lambda match: match.group().split('=')[0] + '=' + redaction,
+            message
+    )
