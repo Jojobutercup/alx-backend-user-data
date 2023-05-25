92e7b274a489b634007770036c417902cfdfb38a
jojobutercup <jojobutercup@gmail.com>
2023-05-25 14:18:19 +0100

    commit

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,22 +3,31 @@
 Filtered Logger module
 """
 
-from typing import List
+import logging
 import re
+from typing import List
 
 
-def filter_datum(
-        fields: List[str],
-        redaction: str,
-        message: str,
-        separator: str
-) -> str:
+class RedactingFormatter(logging.Formatter):
     """
-    Replace field values in a log message with a redaction string
+    Redacting Formatter class
     """
-    regex = r'(' + '|'.join(fields) + r')=[^{}]+'.format(separator)
-    return re.sub(
-            regex,
-            lambda match: match.group().split('=')[0] + '=' + redaction,
-            message
-    )
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super().__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """
+        Format the log record by redacting specified fields
+        """
+        message = record.getMessage()
+        for field in self.fields:
+            regex = r'({})=([^{}]+)'.format(field, self.SEPARATOR)
+            message = re.sub(regex, r'\1=' + self.REDACTION, message)
+        record.message = message
+        return super().format(record)
