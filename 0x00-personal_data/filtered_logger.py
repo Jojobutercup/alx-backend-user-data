df690c216f6d0d41abf1cb30a4ad978138bc88b7
jojobutercup <jojobutercup@gmail.com>
2023-05-25 16:18:09 +0100

    commit

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,8 +4,10 @@ Filtered Logger module
 """
 
 import logging
-import re
+import csv
 from typing import List
+
+PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
 
 
 class RedactingFormatter(logging.Formatter):
@@ -14,20 +16,39 @@ class RedactingFormatter(logging.Formatter):
     """
 
     REDACTION = "***"
-    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
-    SEPARATOR = ";"
-
-    def __init__(self, fields: List[str]):
-        super().__init__(self.FORMAT)
-        self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
         """
         Format the log record by redacting specified fields
         """
-        message = record.getMessage()
-        for field in self.fields:
-            regex = r'({})=([^{}]+)'.format(field, self.SEPARATOR)
-            message = re.sub(regex, r'\1=' + self.REDACTION, message)
-        record.msg = message
-        return super().format(record)
+        message = super().format(record)
+        for field in PII_FIELDS:
+            message = message.replace(field, self.REDACTION)
+        return message
+
+
+def get_logger() -> logging.Logger:
+    """
+    Create and configure a logger object
+    """
+    logger = logging.getLogger('user_data')
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+    formatter = RedactingFormatter(
+            "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s")
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(formatter)
+    logger.addHandler(stream_handler)
+    return logger
+
+
+def read_file(filename: str) -> List[dict]:
+    """
+    Read a CSV file and return its content as a list of dictionaries
+    """
+    data = []
+    with open(filename, 'r') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            data.append(dict(row))
+    return data
